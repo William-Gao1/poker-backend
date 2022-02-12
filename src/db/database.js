24c2a1089194c0063ee08fc426b89bbdfe1b5555
William-Gao1 <william2010.gao@gmail.com>
2022-02-12 17:24:39 -0500

    fix cert error

--- src/db/database.js
+++ src/db/database.js
@@ -5,7 +5,7 @@ const pgp = require("pg-promise")(config)
 const cn = {
     connectionString: process.env.DATABASE_URL,
     max: 20,
-    ssl: process.env.ENV=='PROD'
+    ssl: process.env.ENV=='PROD' ? {rejectUnauthorized: false} : false
 }
 
 const db = pgp(cn)
